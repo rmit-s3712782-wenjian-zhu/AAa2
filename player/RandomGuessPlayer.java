3fb23d560324b5df63a5b46bf795a28da62b536d
Inci <inci90@users.noreply.github.com>
2018-10-04 20:48:30 +1000

    Update RandomGuessPlayer.java

--- player/RandomGuessPlayer.java
+++ player/RandomGuessPlayer.java
@@ -130,7 +130,7 @@ public class RandomGuessPlayer implements Player {
     @Override
     public boolean noRemainingShips() {
         //iterate through ship list and if no coords (health) then return true
-            if shipLocations.size() == 0)
+            if shipLocations.size() == 0){
                 return true;
         }
         return false;
